210078c98f893690898ae7f8877be58ce67da9d5
Bernardo Pombinho <bernardopombinho@.gmail.com>
2025-01-30 03:22:58 +0000

    component class name updated

--- app/View/Components/AddressCountrySelect.php
+++ app/View/Components/AddressCountrySelect.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class address-country-select extends Component
+class AddressCountrySelect extends Component
 {
     /**
      * Create a new component instance.
